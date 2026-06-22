8aeac45b436e597dca692caf0a21512c9f8d354f
Sandev Abeykoon <64116737+BlazeIsClone@users.noreply.github.com>
2026-06-22 21:20:09 +0530

    Add: implement-stack-using-queues - submission-1

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-1.py
@@ -0,0 +1,21 @@
+class MyStack:
+    def __init__(self):
+        self.data = []
+    def push(self, x: int) -> None:
+        self.data.append(x)
+    def pop(self) -> int:
+        return self.data.pop()
+
+    def top(self) -> int:
+        return self.data[-1]
+    def empty(self) -> bool:
+        if len(self.data) >= 1:
+            return False
+        return True
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
